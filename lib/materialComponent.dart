80956771d55185f13bcfbdadc9c2718466f83c3c
nurwhyusuci <nurwahyusucirahayu@gmail.com>
2024-04-05 23:20:58 +0700

    first commit

---
+++ lib/materialComponent.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Wecome to flutter',
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Welcome to Flutter'),
+            ),
+            body: Container(
+                padding: EdgeInsets.all(40),
+                child: Card(
+                    child: Column(mainAxisSize: MainAxisSize.min, children: [
+                  ListTile(
+                    leading: Icon(Icons.people_sharp),
+                    title: Text('Nur Wahyu Suci Rahayu'),
+                    subtitle: Text('@nurwhyusuci'),
+                  ),
+                  Text('This is Card', style: TextStyle(fontSize: 40))
+                ])))));
+  }
+}
